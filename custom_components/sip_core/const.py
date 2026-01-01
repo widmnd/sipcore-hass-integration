d6c815db0feb79e9c6f403a8f6156e8b3971753c
Sergio Soto <me@srgio.es>
2026-01-01 19:16:16 +0100

    Add HASS_DATA_KEY constants

--- custom_components/sip_core/const.py
+++ custom_components/sip_core/const.py
@@ -4,3 +4,9 @@ DOMAIN = "sip_core"
 JS_FILENAME = "sip_core.js"
 JS_URL_PATH = f"/sip_core_files/{JS_FILENAME}"
 ASTERISK_ADDON_SLUG = "3e533915_asterisk"
+
+# Home Assistant data store keys
+HASS_DATA_KEY_CONFIG = "config"
+HASS_DATA_KEY_OPTIONS = "options"
+HASS_DATA_KEY_ENTRY_ID = "entry_id"
+HASS_DATA_KEY_SIP_CONFIG = "sip_config"
