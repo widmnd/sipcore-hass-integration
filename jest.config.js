4643d4401ef6a065c4d3eb4566ecead4b322c68c
Sergio Soto <me@srgio.es>
2026-01-01 20:31:23 +0100

    Remove coverage tests

--- jest.config.js
+++ jest.config.js
@@ -5,18 +5,4 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec).ts'],
   testPathIgnorePatterns: ['test-utils.ts', 'test-fixtures.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    'custom_components/**/*.py',
-    '!src/**/*.d.ts',
-    '!**/__tests__/**',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
-    },
-  },
 };
